8bc4b2749fdb429823053c44894f3c376d0d5faa
AlanSoto31 <soto.alan@hotmail.com>
2021-05-05 08:59:02 -0500

    Limit the number of characters in the player name

--- src/Scenes/GetNameScene.js
+++ src/Scenes/GetNameScene.js
@@ -10,22 +10,24 @@ export default class GetNameScene extends Phaser.Scene {
 
   create(){
     this.model = this.sys.game.globals.model;
-    this.text = this.add.text(60, 20, 'Enter your name', { fontSize: 30 });
+    this.text = this.add.text(20, 20, 'Type your name and press', { fontSize: 25 });
+    this.text = this.add.text(20, 50, 'enter to start the game', { fontSize: 25 });
     this.menuButton = new Button(this, config.width/2, 230, 'blueButton1', 'blueButton2', 'Menu', 'Title');
     
-    let textEntry = this.add.text(10, 50, '', { font: '32px Courier', fill: '#ffff00' });
+    let textEntry = this.add.text(90, 100, '', { font: '32px Courier', fill: '#ffff00' });
 
     this.input.keyboard.on('keydown', (event) => {
         if (event.keyCode === 8 && textEntry.text.length > 0){
             textEntry.text = textEntry.text.substr(0, textEntry.text.length - 1);
         }
-        else if (event.keyCode === 32 || (event.keyCode >= 48 && event.keyCode < 90)){
+        else if (event.keyCode === 32 || (event.keyCode >= 48 && event.keyCode < 90) && textEntry.text.length < 12){
             textEntry.text += event.key;
         }
 
         if(event.code === "Enter" && textEntry.text.length > 0){
           this.model.playerName = textEntry.text;
           //ScoreScene.setGameName();
+
           //ScoreScene.setScore(this.model.playerName, this.model.score);
 
           this.scene.start('WorldScene')
